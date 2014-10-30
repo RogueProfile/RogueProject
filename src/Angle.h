4f2cfd3fdd5164648d4ef5b6686da2b76b6bd611
Tyler Reisinger <reisinger.tyler@gmail.com>
2014-10-30 13:47:07 -0400

    Added Angle.h for radian/degree semantic typing

---
+++ src/Angle.h
@@ -0,0 +1,154 @@
+#ifndef ANGLE_H__
+#define ANGLE_H__
+
+#include <cmath>
+#include <ostream>
+
+#include "Constants.h"
+
+template<typename T>
+class Degrees;
+
+template<typename T>
+class Radians
+{
+public:
+    explicit constexpr Radians(T val);
+    ~Radians() = default;
+
+    constexpr Radians(const Radians& rhs) = default;
+    constexpr Radians(Radians&& rhs) = default;
+    constexpr Radians& operator =(const Radians& rhs) = default;
+    constexpr Radians& operator =(Radians&& rhs) = default;
+
+    constexpr Degrees<T> to_degrees() const;
+
+    T value;
+};
+
+template<typename T>
+class Degrees
+{
+public:
+    explicit constexpr Degrees(T val);
+    ~Degrees() = default;
+
+    constexpr Degrees(const Degrees& rhs) = default;
+    constexpr Degrees(Degrees&& rhs) = default;
+    constexpr Degrees& operator =(const Degrees& rhs) = default;
+    constexpr Degrees& operator =(Degrees&& rhs) = default;
+
+    constexpr Radians<T> to_radians() const;
+
+    T value;
+};
+
+template<typename T>
+std::ostream& operator <<(std::ostream& stream, Radians<T> rhs)
+{
+    stream << rhs.value;
+    return stream;
+}
+
+template<typename T>
+std::ostream& operator <<(std::ostream& stream, Degrees<T> rhs)
+{
+    stream << rhs.value;
+    return stream;
+}
+
+template<typename T>
+Radians<T> sin(Radians<T> arg)
+{
+    using std::sin;
+    return Radians<T>(sin(arg.value));
+}
+
+template<typename T>
+Degrees<T> sin(Degrees<T> arg)
+{
+    using std::sin;
+    return Radians<T>(sin(arg.to_radians().value)).to_degrees();
+}
+
+template<typename T>
+Radians<T> cos(Radians<T> arg)
+{
+    using std::cos;
+    return Radians<T>(cos(arg.value));
+}
+
+template<typename T>
+Degrees<T> cos(Degrees<T> arg)
+{
+    using std::cos;
+    return Radians<T>(cos(arg.to_radians().value)).to_degrees();
+}
+
+template<typename T>
+Radians<T> tan(Radians<T> arg)
+{
+    using std::tan;
+    return Radians<T>(tan(arg.value));
+}
+
+template<typename T>
+Degrees<T> tan(Degrees<T> arg)
+{
+    using std::tan;
+    return Radians<T>(tan(arg.to_radians().value)).to_degrees();
+}
+
+template<typename T>
+Radians<T> asin(T arg)
+{
+    using std::asin;
+    return Radians<T>(asin(arg));
+}
+
+template<typename T>
+Radians<T> acos(T arg)
+{
+    using std::acos;
+    return Radians<T>(acos(arg));
+}
+
+template<typename T>
+Radians<T> atan(T arg)
+{
+    using std::atan;
+    return Radians<T>(atan(arg));
+}
+
+template<typename T>
+Radians<T> atan2(T x, T y)
+{
+    using std::atan2;
+    return Radians<T>(atan2(y, x));
+}
+
+template<typename T>
+inline constexpr Radians<T>::Radians(T val):
+    value(val)
+{
+}
+
+template<typename T>
+inline constexpr Degrees<T> Radians<T>::to_degrees() const
+{
+    return Degrees<T>(value * (T(180) / PI<T>)); 
+}
+ 
+template<typename T>
+inline constexpr Degrees<T>::Degrees(T val):
+    value(val)
+{
+}
+ 
+template<typename T>
+inline constexpr Radians<T> Degrees<T>::to_radians() const
+{
+    return Radians<T>(value * (PI<T> / T(180))); 
+}
+ 
+#endif
