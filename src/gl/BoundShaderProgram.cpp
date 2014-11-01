11df88c14790a73922e89bd3aef9fc5c5809610e
Tyler1800 <reisinger.tyler@gmail.com>
2014-10-31 23:22:28 -0400

    Implemented current uniform methods for BoundShaderProgram

--- src/gl/BoundShaderProgram.cpp
+++ src/gl/BoundShaderProgram.cpp
@@ -1,5 +1,8 @@
 #include "BoundShaderProgram.h"
 
+#include "GlMacros.h"
+#include "GlException.h"
+
 namespace gl
 {
 
@@ -23,4 +26,203 @@ BoundShaderProgram& BoundShaderProgram::operator=(BoundShaderProgram&& other) no
     return *this;
 }
 
+void BoundShaderProgram::set_uniform_value(int location, int value)
+{
+    glUniform1i(location, value);
+    CHECK_GL_ERROR(glUniform1i); 
 }
+ 
+void BoundShaderProgram::set_uniform_value(int location, unsigned int value)
+{
+    glUniform1ui(location, value);
+    CHECK_GL_ERROR(glUniform1ui); 
+}
+ 
+void BoundShaderProgram::set_uniform_value(int location, float value)
+{
+    glUniform1f(location, value);
+    CHECK_GL_ERROR(glUniform1f); 
+}
+ 
+void BoundShaderProgram::set_uniform_value(int location, int value1, int value2)
+{
+    glUniform2i(location, value1, value2); 
+    CHECK_GL_ERROR(glUniform2i);
+}
+ 
+void BoundShaderProgram::set_uniform_value(int location, unsigned int value1, unsigned int value2)
+{
+    glUniform2ui(location, value1, value2); 
+    CHECK_GL_ERROR(glUniform2ui);
+}
+ 
+void BoundShaderProgram::set_uniform_value(int location, float value1, float value2)
+{
+    glUniform2f(location, value1, value2); 
+    CHECK_GL_ERROR(glUniform2f);
+}
+ 
+void BoundShaderProgram::set_uniform_value(int location, int value1, int value2, int value3)
+{
+    glUniform3i(location, value1, value2, value3); 
+    CHECK_GL_ERROR(glUniform3i);
+}
+ 
+void BoundShaderProgram::set_uniform_value(int location, unsigned int value1, unsigned int value2, unsigned int value3)
+{
+    glUniform3ui(location, value1, value2, value3); 
+    CHECK_GL_ERROR(glUniform3ui);
+}
+ 
+void BoundShaderProgram::set_uniform_value(int location, float value1, float value2, float value3)
+{
+    glUniform3f(location, value1, value2, value3); 
+    CHECK_GL_ERROR(glUniform3f);
+}
+ 
+void BoundShaderProgram::set_uniform_value(int location, int value1, int value2, int value3, int value4)
+{
+    glUniform4i(location, value1, value2, value3, value4); 
+    CHECK_GL_ERROR(glUniform4i);
+}
+ 
+void BoundShaderProgram::set_uniform_value(int location, unsigned int value1, unsigned int value2, unsigned int value3, unsigned int value4)
+{
+    glUniform4ui(location, value1, value2, value3, value4); 
+    CHECK_GL_ERROR(glUniform4ui);
+}
+ 
+void BoundShaderProgram::set_uniform_value(int location, float value1, float value2, float value3, float value4)
+{
+    glUniform4f(location, value1, value2, value3, value4); 
+    CHECK_GL_ERROR(glUniform4f);
+}
+ 
+void BoundShaderProgram::set_uniform_value(int location, const Vector2i& value)
+{
+    glUniform2i(location, value.x, value.y); 
+    CHECK_GL_ERROR(glUniform2i);
+}
+ 
+void BoundShaderProgram::set_uniform_value(int location, const Vector2ui& value)
+{
+    glUniform2ui(location, value.x, value.y); 
+    CHECK_GL_ERROR(glUniform2ui);
+}
+ 
+void BoundShaderProgram::set_uniform_value(int location, const Vector2f& value)
+{
+    glUniform2f(location, value.x, value.y); 
+    CHECK_GL_ERROR(glUniform2f);
+}
+ 
+void BoundShaderProgram::set_uniform_value_matrix2(int location, const float* values, bool transpose)
+{
+    glUniformMatrix2fv(location, 1, transpose == true ? 1 : 0, values);
+    CHECK_GL_ERROR(glUniformMatrix2fv);
+}
+ 
+void BoundShaderProgram::set_uniform_value_matrix2(int location, const std::array<float, 4>& values, bool transpose)
+{
+    glUniformMatrix2fv(location, 1, transpose == true ? 1 : 0, values.data()); 
+    CHECK_GL_ERROR(glUniformMatrix2fv);
+}
+ 
+void BoundShaderProgram::set_uniform_value_matrix3(int location, const float* values, bool transpose)
+{
+    glUniformMatrix3fv(location, 1, transpose == true ? 1 : 0, values);
+    CHECK_GL_ERROR(glUniformMatrix3fv);
+}
+ 
+void BoundShaderProgram::set_uniform_value_matrix3(int location, const std::array<float, 9>& values, bool transpose)
+{
+    glUniformMatrix3fv(location, 1, transpose == true ? 1 : 0, values.data()); 
+    CHECK_GL_ERROR(glUniformMatrix3fv);
+}
+ 
+void BoundShaderProgram::set_uniform_value_matrix4(int location, const float* values, bool transpose)
+{
+    glUniformMatrix4fv(location, 1, transpose == true ? 1 : 0, values);
+    CHECK_GL_ERROR(glUniformMatrix4f);
+}
+ 
+void BoundShaderProgram::set_uniform_value_matrix4(int location, const std::array<float, 16>& values, bool transpose)
+{
+    glUniformMatrix4fv(location, 1, transpose == true ? 1 : 0, values.data()); 
+    CHECK_GL_ERROR(glUniformMatrix4fv);
+}
+ 
+void BoundShaderProgram::set_uniform_value_array1(int location, const int* values, size_t count)
+{
+    glUniform1iv(location, count, values); 
+    CHECK_GL_ERROR(glUniform1iv);
+}
+ 
+void BoundShaderProgram::set_uniform_value_array1(int location, const unsigned int* values, size_t count)
+{
+    glUniform1uiv(location, count, values); 
+    CHECK_GL_ERROR(glUniform1uiv);
+}
+ 
+void BoundShaderProgram::set_uniform_value_array1(int location, const float* values, size_t count)
+{
+    glUniform1fv(location, count, values); 
+    CHECK_GL_ERROR(glUniform1fv);
+}
+
+void BoundShaderProgram::set_uniform_value_array2(int location, const int* values, size_t count)
+{
+    glUniform2iv(location, count, values); 
+    CHECK_GL_ERROR(glUniform2iv);
+}
+ 
+void BoundShaderProgram::set_uniform_value_array2(int location, const unsigned int* values, size_t count)
+{
+    glUniform2uiv(location, count, values); 
+    CHECK_GL_ERROR(glUniform2uiv);
+}
+ 
+void BoundShaderProgram::set_uniform_value_array2(int location, const float* values, size_t count)
+{
+    glUniform2fv(location, count, values); 
+    CHECK_GL_ERROR(glUniform2fv);
+}
+ 
+void BoundShaderProgram::set_uniform_value_array3(int location, const int* values, size_t count)
+{
+    glUniform3iv(location, count, values); 
+    CHECK_GL_ERROR(glUniform3iv);
+}
+ 
+void BoundShaderProgram::set_uniform_value_array3(int location, const unsigned int* values, size_t count)
+{
+    glUniform3uiv(location, count, values); 
+    CHECK_GL_ERROR(glUniform3uiv);
+}
+ 
+void BoundShaderProgram::set_uniform_value_array3(int location, const float* values, size_t count)
+{
+    glUniform3fv(location, count, values); 
+    CHECK_GL_ERROR(glUniform3fv);
+}
+ 
+void BoundShaderProgram::set_uniform_value_array4(int location, const int* values, size_t count)
+{
+    glUniform4iv(location, count, values); 
+    CHECK_GL_ERROR(glUniform4iv);
+}
+ 
+void BoundShaderProgram::set_uniform_value_array4(int location, const unsigned int* values, size_t count)
+{
+    glUniform4uiv(location, count, values); 
+    CHECK_GL_ERROR(glUniform4uiv);
+}
+ 
+void BoundShaderProgram::set_uniform_value_array4(int location, const float* values, size_t count)
+{
+    glUniform4fv(location, count, values); 
+    CHECK_GL_ERROR(glUniform4fv);
+}
+ 
+}
+ 
--- src/gl/BoundShaderProgram.h
+++ src/gl/BoundShaderProgram.h
@@ -4,6 +4,9 @@
 #include "ShaderProgram.h"
 #include "TargetLock.h"
 
+
+#include "Vector2.h"
+
 namespace gl
 {
 
@@ -19,6 +22,99 @@ public:
     BoundShaderProgram& operator =(const BoundShaderProgram& other) = delete;
     BoundShaderProgram& operator =(BoundShaderProgram&& other) noexcept;
 
+    void set_uniform_value(int location, int value);
+    void set_uniform_value(int location, unsigned int value);
+    void set_uniform_value(int location, float value);
+    void set_uniform_value(int location, int value1, int value2);
+    void set_uniform_value(int location, unsigned int value1, unsigned int value2);
+    void set_uniform_value(int location, float value1, float value2);
+    void set_uniform_value(int location, int value1, int value2, int value3);
+    void set_uniform_value(int location, unsigned int value1, unsigned int value2, unsigned int value3);
+    void set_uniform_value(int location, float value1, float value2, float value3);
+    void set_uniform_value(int location, int value1, int value2, int value3, int value4);
+    void set_uniform_value(int location, unsigned int value1, unsigned int value2, unsigned int value3, unsigned int value4);
+    void set_uniform_value(int location, float value1, float value2, float value3, float value4);
+
+    void set_uniform_value(int location, const Vector2i& value);
+    void set_uniform_value(int location, const Vector2ui& value);
+    void set_uniform_value(int location, const Vector2f& value);
+    //TODO: Vector3
+    //TODO: Vector4
+    //TODO: Matrix2
+    //TODO: Matrix3
+    //TODO: Matrix4
+
+    void set_uniform_value(const char* uniform_name, int value);
+    void set_uniform_value(const char* uniform_name, unsigned int value);
+    void set_uniform_value(const char* uniform_name, float value);
+    void set_uniform_value(const char* uniform_name, int value1, int value2);
+    void set_uniform_value(const char* uniform_name, unsigned int value1, unsigned int value2);
+    void set_uniform_value(const char* uniform_name, float value1, float value2);
+    void set_uniform_value(const char* uniform_name, int value1, int value2, int value3);
+    void set_uniform_value(const char* uniform_name, unsigned int value1, unsigned int value2, unsigned int value3);
+    void set_uniform_value(const char* uniform_name, float value1, float value2, float value3);
+    void set_uniform_value(const char* uniform_name, int value1, int value2, int value3, int value4);
+    void set_uniform_value(const char* uniform_name, unsigned int value1, unsigned int value2, unsigned int value3, unsigned int value4);
+    void set_uniform_value(const char* uniform_name, float value1, float value2, float value3, float value4);
+
+    void set_uniform_value(const char* uniform_name, const Vector2i& value);
+    void set_uniform_value(const char* uniform_name, const Vector2ui& value);
+    void set_uniform_value(const char* uniform_name, const Vector2f& value);
+
+    void set_uniform_value_matrix2(int location, const float* values, bool transpose=false);
+    void set_uniform_value_matrix2(int location, const std::array<float, 4>& values, bool transpose=false);
+    void set_uniform_value_matrix3(int location, const float* values, bool transpose=false);
+    void set_uniform_value_matrix3(int location, const std::array<float, 9>& values, bool transpose=false);
+    void set_uniform_value_matrix4(int location, const float* values, bool transpose=false);
+    void set_uniform_value_matrix4(int location, const std::array<float, 16>& values, bool transpose=false);
+
+    void set_uniform_value_matrix2(const char* uniform_name, const float* values, bool transpose=false);
+    void set_uniform_value_matrix2(const char* uniform_name, const std::array<float, 4>& values, bool transpose=false);
+    void set_uniform_value_matrix3(const char* uniform_name, const float* values, bool transpose=false);
+    void set_uniform_value_matrix3(const char* uniform_name, const std::array<float, 9>& values, bool transpose=false);
+    void set_uniform_value_matrix4(const char* uniform_name, const float* values, bool transpose=false);
+    void set_uniform_value_matrix4(const char* uniform_name, const std::array<float, 16>& values, bool transpose=false);
+
+    void set_uniform_value_array1(int location, const int* values, size_t count);
+    void set_uniform_value_array1(int location, const unsigned int* values, size_t count);
+    void set_uniform_value_array1(int location, const float* values, size_t count);
+    template<size_t N>
+    void set_uniform_value_array1(int location, const std::array<int, N>& values);
+    template<size_t N>
+    void set_uniform_value_array1(int location, const std::array<unsigned int, N>& values);
+    template<size_t N>
+    void set_uniform_value_array1(int location, const std::array<float, N>& values);
+
+    void set_uniform_value_array2(int location, const int* values, size_t count);
+    void set_uniform_value_array2(int location, const unsigned int* values, size_t count);
+    void set_uniform_value_array2(int location, const float* values, size_t count);
+    template<size_t N>
+    void set_uniform_value_array2(int location, const std::array<int, N>& values);
+    template<size_t N>
+    void set_uniform_value_array2(int location, const std::array<unsigned int, N>& values);
+    template<size_t N>
+    void set_uniform_value_array2(int location, const std::array<float, N>& values);
+
+    void set_uniform_value_array3(int location, const int* values, size_t count);
+    void set_uniform_value_array3(int location, const unsigned int* values, size_t count);
+    void set_uniform_value_array3(int location, const float* values, size_t count);
+    template<size_t N>
+    void set_uniform_value_array3(int location, const std::array<int, N>& values);
+    template<size_t N>
+    void set_uniform_value_array3(int location, const std::array<unsigned int, N>& values);
+    template<size_t N>
+    void set_uniform_value_array3(int location, const std::array<float, N>& values);
+
+    void set_uniform_value_array4(int location, const int* values, size_t count);
+    void set_uniform_value_array4(int location, const unsigned int* values, size_t count);
+    void set_uniform_value_array4(int location, const float* values, size_t count);
+    template<size_t N>
+    void set_uniform_value_array4(int location, const std::array<int, N>& values);
+    template<size_t N>
+    void set_uniform_value_array4(int location, const std::array<unsigned int, N>& values);
+    template<size_t N>
+    void set_uniform_value_array4(int location, const std::array<float, N>& values);
+
 protected:
     
     ShaderProgram* m_object = nullptr;
@@ -26,7 +122,184 @@ protected:
 private:
 };
 
+inline void BoundShaderProgram::set_uniform_value(const char* uniform_name, int value)
+{
+    set_uniform_value(m_object->get_uniform_location(uniform_name), value); 
+}
+ 
+inline void BoundShaderProgram::set_uniform_value(const char* uniform_name, unsigned int value)
+{
+    set_uniform_value(m_object->get_uniform_location(uniform_name), value); 
+}
+ 
+inline void BoundShaderProgram::set_uniform_value(const char* uniform_name, float value)
+{
+    set_uniform_value(m_object->get_uniform_location(uniform_name), value); 
+}
+ 
+inline void BoundShaderProgram::set_uniform_value(const char* uniform_name, int value1, int value2)
+{
+    set_uniform_value(m_object->get_uniform_location(uniform_name), value1, value2); 
+}
+ 
+inline void BoundShaderProgram::set_uniform_value(const char* uniform_name, unsigned int value1, unsigned int value2)
+{
+    set_uniform_value(m_object->get_uniform_location(uniform_name), value1, value2); 
+}
+ 
+inline void BoundShaderProgram::set_uniform_value(const char* uniform_name, float value1, float value2)
+{
+    set_uniform_value(m_object->get_uniform_location(uniform_name), value1, value2); 
+}
+ 
+inline void BoundShaderProgram::set_uniform_value(const char* uniform_name, int value1, int value2, int value3)
+{
+    set_uniform_value(m_object->get_uniform_location(uniform_name), value1, value2, value3); 
+}
+ 
+inline void BoundShaderProgram::set_uniform_value(const char* uniform_name, unsigned int value1, unsigned int value2, unsigned int value3)
+{
+ 
+    set_uniform_value(m_object->get_uniform_location(uniform_name), value1, value2, value3); 
+}
+ 
+inline void BoundShaderProgram::set_uniform_value(const char* uniform_name, float value1, float value2, float value3)
+{
+    set_uniform_value(m_object->get_uniform_location(uniform_name), value1, value2, value3); 
+}
+ 
+inline void BoundShaderProgram::set_uniform_value(const char* uniform_name, int value1, int value2, int value3, int value4)
+{
+    set_uniform_value(m_object->get_uniform_location(uniform_name), value1, value2, value3, value4); 
+}
+ 
+inline void BoundShaderProgram::set_uniform_value(const char* uniform_name, unsigned int value1, unsigned int value2, unsigned int value3, unsigned int value4)
+{
+    set_uniform_value(m_object->get_uniform_location(uniform_name), value1, value2, value3, value4); 
+}
+ 
+inline void BoundShaderProgram::set_uniform_value(const char* uniform_name, float value1, float value2, float value3, float value4)
+{
+    set_uniform_value(m_object->get_uniform_location(uniform_name), value1, value2, value3, value4); 
 }
 
+inline void BoundShaderProgram::set_uniform_value(const char* uniform_name, const Vector2i& value)
+{
+    set_uniform_value(m_object->get_uniform_location(uniform_name), value); 
+}
+ 
+inline void BoundShaderProgram::set_uniform_value(const char* uniform_name, const Vector2ui& value)
+{
+    set_uniform_value(m_object->get_uniform_location(uniform_name), value); 
+}
+ 
+inline void BoundShaderProgram::set_uniform_value(const char* uniform_name, const Vector2f& value)
+{
+    set_uniform_value(m_object->get_uniform_location(uniform_name), value); 
+}
+ 
+inline void BoundShaderProgram::set_uniform_value_matrix2(const char* uniform_name, const float* values, bool transpose)
+{
+    set_uniform_value_matrix2(m_object->get_uniform_location(uniform_name), values, transpose);
+}
+
+inline void BoundShaderProgram::set_uniform_value_matrix2(const char* uniform_name, const std::array<float, 4>& values, bool transpose)
+{
+    set_uniform_value_matrix2(m_object->get_uniform_location(uniform_name), values, transpose);
+}
+ 
+inline void BoundShaderProgram::set_uniform_value_matrix3(const char* uniform_name, const float* values, bool transpose)
+{
+    set_uniform_value_matrix3(m_object->get_uniform_location(uniform_name), values, transpose);
+}
+ 
+inline void BoundShaderProgram::set_uniform_value_matrix3(const char* uniform_name, const std::array<float, 9>& values, bool transpose)
+{
+    set_uniform_value_matrix3(m_object->get_uniform_location(uniform_name), values, transpose);
+}
+ 
+inline void BoundShaderProgram::set_uniform_value_matrix4(const char* uniform_name, const float* values, bool transpose)
+{
+    set_uniform_value_matrix4(m_object->get_uniform_location(uniform_name), values, transpose);
+}
+ 
+inline void BoundShaderProgram::set_uniform_value_matrix4(const char* uniform_name, const std::array<float, 16>& values, bool transpose)
+{
+    set_uniform_value_matrix4(m_object->get_uniform_location(uniform_name), values, transpose);
+}
+
+template<size_t N>
+inline void BoundShaderProgram::set_uniform_value_array1(int location, const std::array<int, N>& values)
+{
+    set_uniform_array1(location, values.data(), N); 
+}
+
+template<size_t N>
+inline void BoundShaderProgram::set_uniform_value_array1(int location, const std::array<unsigned int, N>& values)
+{
+    set_uniform_array1(location, values.data(), N); 
+}
+
+template<size_t N>
+inline void BoundShaderProgram::set_uniform_value_array1(int location, const std::array<float, N>& values)
+{
+    set_uniform_array1(location, values.data(), N); 
+}
+template<size_t N>
+inline void BoundShaderProgram::set_uniform_value_array2(int location, const std::array<int, N>& values)
+{
+    set_uniform_array2(location, values.data(), N/2); 
+}
+
+template<size_t N>
+inline void BoundShaderProgram::set_uniform_value_array2(int location, const std::array<unsigned int, N>& values)
+{
+    set_uniform_array2(location, values.data(), N/2); 
+}
+
+template<size_t N>
+inline void BoundShaderProgram::set_uniform_value_array2(int location, const std::array<float, N>& values)
+{
+    set_uniform_array2(location, values.data(), N/2); 
+}
+
+template<size_t N>
+inline void BoundShaderProgram::set_uniform_value_array3(int location, const std::array<int, N>& values)
+{
+    set_uniform_array3(location, values.data(), N/3); 
+}
+
+template<size_t N>
+inline void BoundShaderProgram::set_uniform_value_array3(int location, const std::array<unsigned int, N>& values)
+{
+    set_uniform_array3(location, values.data(), N/3); 
+}
+
+template<size_t N>
+inline void BoundShaderProgram::set_uniform_value_array3(int location, const std::array<float, N>& values)
+{
+    set_uniform_array3(location, values.data(), N/3); 
+}
+
+template<size_t N>
+inline void BoundShaderProgram::set_uniform_value_array4(int location, const std::array<int, N>& values)
+{
+    set_uniform_array4(location, values.data(), N/4); 
+}
+
+template<size_t N>
+inline void BoundShaderProgram::set_uniform_value_array4(int location, const std::array<unsigned int, N>& values)
+{
+    set_uniform_array4(location, values.data(), N/4); 
+}
+
+template<size_t N>
+inline void BoundShaderProgram::set_uniform_value_array4(int location, const std::array<float, N>& values)
+{
+    set_uniform_array4(location, values.data(), N/4); 
+}
+ 
+}
+ 
 #endif
     
