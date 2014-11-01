2d85f05702c482c5e5c009ae17dfa45279227587
Twistedlink07 <cunruh3760@gmail.com>
2014-11-01 16:30:59 +0000

    Created HsvColor class

---
+++ src/HsvColor.h
@@ -0,0 +1,34 @@
+#ifndef HSVCOLOR_H__
+#define HSVCOLOR_H__
+
+#include "Angle.h"
+
+class HsvColor
+{
+public:
+    constexpr HsvColor(Degrees<float> hue_value, float saturation_value, float value_value, float alpha_value=1)
+        hue(hue_value), saturation(saturation_value), value(value_value), alpha(alpha_value)
+    {}
+    constexpr ~HsvColor() = default;
+
+    constexpr HsvColor(const HsvColor& other) = default;
+    constexpr HsvColor(HsvColor&& other) noexcept = default;
+    constexpr HsvColor& operator =(const HsvColor& other) = default;
+    constexpr HsvColor& operator =(HsvColor&& other) noexcept = default;
+
+    constexpr Color to_rgb();
+    static constexpr HsvColor from_rgb(const Color& that); 
+
+    Degrees<float> hue;
+    float saturation;
+    float value;
+    float alpha;
+
+
+protected:
+private:
+};
+
+
+#endif
+    
