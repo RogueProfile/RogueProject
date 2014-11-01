06dafa69338cc66307ca3ce71a5ab133d9b50582
Tyler Reisinger <reisinger.tyler@gmail.com>
2014-10-31 20:36:28 -0400

    Throw TargetBindError in GlContext::bind_shader_program

--- src/gl/GlContext.cpp
+++ src/gl/GlContext.cpp
@@ -2,6 +2,8 @@
 
 #include <iostream>
 
+#include "TargetBindError.h"
+
 namespace gl
 {
 
@@ -12,6 +14,10 @@ void GlContext::clear(const Flags<ClearTarget>& buffers)
 
 BoundShaderProgram GlContext::bind_shader_program(ShaderProgram* program)
 {
+    if(m_bound_shader_program != nullptr)
+    {
+        throw TargetBindError(Target::ShaderProgram);
+    }
     glUseProgram(program->handle());
     return BoundShaderProgram(program, TargetLock(program, 
         &m_bound_shader_program));
