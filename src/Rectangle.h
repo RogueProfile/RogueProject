aaceb4ca03b9b20ffc939a0b125078cc6d3ee2b6
Tyler1800 <reisinger.tyler@gmail.com>
2014-11-02 22:13:23 -0500

    Fixed Rectangle to use the correct origin point

--- src/Rectangle.h
+++ src/Rectangle.h
@@ -8,8 +8,8 @@ class Rectangle
 {
 public:
     constexpr Rectangle();
-    constexpr Rectangle(T right, T top, T width, T height);
-    constexpr Rectangle(const Vector2<T>& top_right, const Vector2<T>& bottom_left);
+    constexpr Rectangle(T left, T bottom, T width, T height);
+    constexpr Rectangle(const Vector2<T>& bottom_left, const Vector2<T>& top_right);
     ~Rectangle() = default;
 
     Rectangle(const Rectangle& other) = default;
@@ -20,12 +20,12 @@ public:
     constexpr bool operator ==(const Rectangle& rhs) const;
     constexpr bool operator !=(const Rectangle& rhs) const;
 
-    constexpr T right() const {return m_right;}
-    constexpr T top() const {return m_top;}
+    constexpr T left() const {return m_left;}
+    constexpr T bottom() const {return m_bottom;}
     constexpr T width() const {return m_width;}
     constexpr T height() const {return m_height;}
-    constexpr T left() const {return right()+width();}
-    constexpr T bottom() const {return top()+height();}
+    constexpr T right() const {return left()+width();}
+    constexpr T top() const {return bottom()+height();}
 
     void offset(T dx, T dy);
     void offset(const Vector2<T>& distance);
@@ -34,9 +34,11 @@ public:
 
     static constexpr Rectangle<T> zero() {return Rectangle<T>(T(0), T(0), T(0), T(0));}
 
+    static constexpr Rectangle<T> from_vertices(T left, T bottom, T right, T top);
+
 protected:
-    T m_right;
-    T m_top;
+    T m_left;
+    T m_bottom;
     T m_width;
     T m_height;
 };
@@ -47,28 +49,28 @@ using Rectanglei = Rectangle<int>;
 
 template<typename T>
 inline constexpr Rectangle<T>::Rectangle():
-    m_right(T(0)), m_top(T(0)), m_width(T(0)), m_height(T(0))
+    m_left(T(0)), m_bottom(T(0)), m_width(T(0)), m_height(T(0))
 { 
 }
 
 template<typename T>
-inline constexpr Rectangle<T>::Rectangle(T right, T top, T width, T height):
-    m_right(right), m_top(top), m_width(width), m_height(height)
+inline constexpr Rectangle<T>::Rectangle(T left, T bottom, T width, T height):
+    m_left(left), m_bottom(bottom), m_width(width), m_height(height)
 {
 }
  
  
 template<typename T>
-inline constexpr Rectangle<T>::Rectangle(const Vector2<T>& top_right, const Vector2<T>& bottom_left):
-    m_right(top_right.x), m_top(top_right.y), m_width(bottom_left.x - top_right.x),
-    m_height(bottom_left.y - top_right.y)
+inline constexpr Rectangle<T>::Rectangle(const Vector2<T>& bottom_left, const Vector2<T>& top_right):
+    m_left(bottom_left.x), m_bottom(bottom_left.y), m_width(top_right.x - bottom_left.x),
+    m_height(top_right.y - bottom_left.y)
 {
 }
  
 template<typename T>
 inline constexpr bool Rectangle<T>::operator==(const Rectangle<T>& rhs) const
 {
-    return (m_right == rhs.m_right && m_top == rhs.m_top && m_width = rhs.m_width
+    return (m_left == rhs.m_left && m_bottom == rhs.m_bottom && m_width = rhs.m_width
             && m_height == rhs.m_height);
 }
  
@@ -81,15 +83,21 @@ inline constexpr bool Rectangle<T>::operator!=(const Rectangle<T>& rhs) const
 template<typename T>
 inline void Rectangle<T>::offset(T dx, T dy)
 {
-    m_right += dx;
-    m_top += dy; 
+    m_left += dx;
+    m_bottom += dy; 
 }
  
 template<typename T>
 inline void Rectangle<T>::offset(const Vector2<T>& distance)
 {
-    m_right += distance.x;
-    m_top += distance.y; 
+    m_left += distance.x;
+    m_bottom += distance.y; 
+}
+ 
+template<typename T>
+inline constexpr Rectangle<T> Rectangle<T>::from_vertices(T left, T bottom, T right, T top)
+{
+    return Rectangle<T>(left, bottom, top - bottom, right - left); 
 }
  
 #endif
